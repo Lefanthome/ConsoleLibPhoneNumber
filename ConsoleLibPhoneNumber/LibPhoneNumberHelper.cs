60fbb85cc95cf5435ddcb771d74fe3072f8e17ae
Ludovic HUSSON <54002@supinfo.com>
2017-12-12 16:34:24 +0100

    Maj du projet avec l'ajout de libPhoneNumber

---
+++ ConsoleLibPhoneNumber/LibPhoneNumberHelper.cs
@@ -0,0 +1,69 @@
+﻿using PhoneNumbers;
+using System.Collections.Generic;
+
+namespace ConsoleLibPhoneNumber
+{
+    public static class LibPhoneNumberHelper
+    {
+        public static bool IsValidPhoneNumber(string phoneNumber, string defaultRegion = "")
+        {
+            try
+            {
+                //Création de l'intance PhoneNumberUtil
+                var util = PhoneNumberUtil.GetInstance();
+                PhoneNumber number = null;
+                //Si le numéro contient l'indicatif + ou le 00
+                if (phoneNumber.StartsWith("+") || phoneNumber.StartsWith("00"))
+                {
+                    if (phoneNumber.StartsWith("00"))
+                    {
+                        phoneNumber = "+" + phoneNumber.Remove(0, 2);
+                    }
+
+                    number = util.Parse(phoneNumber, "");
+                    // Récupération de la région au numéro avec l'indication +
+                    string regionCode = util.GetRegionCodeForNumber(number);
+                    // Validation du numéro qui correspond à la région trouvées
+                    return util.IsValidNumberForRegion(number, regionCode);
+                }
+                else
+                {
+                    number = util.Parse(phoneNumber, defaultRegion);
+                    // Validation du numéro sans indication mais avec le region code
+                    return util.IsValidNumber(number);
+                }
+            }
+            catch (NumberParseException)
+            {
+                //LOG
+                return false;
+            }
+        }
+
+        public static string FormatInternational(string phoneNumber, string regionCode)
+        {
+            if (string.IsNullOrEmpty(phoneNumber)) return string.Empty;
+
+            PhoneNumber number = null;
+            var util = PhoneNumberUtil.GetInstance();
+            number = util.Parse(phoneNumber, regionCode);
+            return util.Format(number, PhoneNumberFormat.INTERNATIONAL);
+        }
+
+        public static List<(string RegionCode, string CountryCode, string PhoneNumber)> GetSupportedRegions()
+        {
+            var list = new List<(string RegionCode, string CountryCode, string PhoneNumber)>();
+            var util = PhoneNumberUtil.GetInstance();
+            var regions = util.GetSupportedRegions();
+
+            foreach (var regionCode in regions)
+            {
+                var phoneNumber = util.GetExampleNumberForType(regionCode, PhoneNumberType.FIXED_LINE_OR_MOBILE);
+                list.Add((regionCode, phoneNumber.CountryCode.ToString(), phoneNumber.NationalNumber.ToString()));
+            }
+
+            return list;
+        }
+
+    }
+}
--- ConsoleLibPhoneNumber/Program.cs
+++ ConsoleLibPhoneNumber/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleLibPhoneNumber
 {
@@ -6,7 +8,28 @@ namespace ConsoleLibPhoneNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("PHONE NUMBER");
+
+            Console.WriteLine("---------------------");
+            Console.WriteLine("1-VALIDATION");
+            Console.WriteLine($"Phone: 0800503020 - Region: FR - RESULT:{LibPhoneNumberHelper.IsValidPhoneNumber("0800503020", "FR")}");
+            Console.WriteLine($"Phone: 0262990102 - Region: FR - RESULT:{LibPhoneNumberHelper.IsValidPhoneNumber("0262990102", "FR")}");
+            Console.WriteLine($"Phone: +33647631289 - Region: FR - RESULT:{LibPhoneNumberHelper.IsValidPhoneNumber("+33647631289")}");
+
+            Console.WriteLine("---------------------");
+            Console.WriteLine("2-FORMAT INTERNATIONAL");
+            Console.WriteLine($"Phone: 0800503020 - Region: FR - RESULT:{LibPhoneNumberHelper.FormatInternational("0800503020", "FR")}");
+
+            Console.WriteLine("---------------------");
+            Console.WriteLine("3-PHONE NUMBER REGION - 10 First");
+
+            IEnumerable<(string RegionCode, string CountryCode, string PhoneNumber)> PhoneRegions = LibPhoneNumberHelper.GetSupportedRegions().Take(10);
+
+            foreach(var phoneRegion in PhoneRegions)
+            {
+                Console.WriteLine($"Region Code: {phoneRegion.RegionCode} - Country Code: {phoneRegion.CountryCode} - Phone: {phoneRegion.PhoneNumber}");
+            }
+            Console.Read();
         }
     }
 }
