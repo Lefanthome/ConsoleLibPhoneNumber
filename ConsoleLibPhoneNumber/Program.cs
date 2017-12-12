fcd0a55a7a57fe994e1dc4a1fdf607559fec0fa6
Ludovic HUSSON <54002@supinfo.com>
2017-12-12 14:28:13 +0100

    Add project files.

---
+++ ConsoleLibPhoneNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleLibPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
